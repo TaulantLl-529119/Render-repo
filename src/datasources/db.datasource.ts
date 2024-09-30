9750cdc093f4c65786712fa3f6f2d6e274e3ebdf
TaulantLl-529119 <529119@student.fontys.nl>
2024-09-30 15:13:49 +0200

    omething 	# Please enter the commit message for your changes. Lines starting

--- src/datasources/db.datasource.ts
+++ src/datasources/db.datasource.ts
@@ -1,5 +1,5 @@
-import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
-import {juggler} from '@loopback/repository';
+// import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+// import {juggler} from '@loopback/repository';
 
 // const config = {
 //   name: 'db',
@@ -8,20 +8,20 @@ import {juggler} from '@loopback/repository';
 //   file: './data/db.json'
 // };
 
-// Observe application's life cycle to disconnect the datasource when
-// application is stopped. This allows the application to be shut down
-// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
-// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
-@lifeCycleObserver('datasource')
-export class DbDataSource extends juggler.DataSource
-  implements LifeCycleObserver {
-  static dataSourceName = 'db';
-  static readonly defaultConfig = config;
+// // Observe application's life cycle to disconnect the datasource when
+// // application is stopped. This allows the application to be shut down
+// // gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// // Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+// @lifeCycleObserver('datasource')
+// export class DbDataSource extends juggler.DataSource
+//   implements LifeCycleObserver {
+//   static dataSourceName = 'db';
+//   static readonly defaultConfig = config;
 
-  constructor(
-    @inject('datasources.config.db', {optional: true})
-    dsConfig: object = config,
-  ) {
-    super(dsConfig);
-  }
-}
+//   constructor(
+//     @inject('datasources.config.db', {optional: true})
+//     dsConfig: object = config,
+//   ) {
+//     super(dsConfig);
+//   }
+// }
