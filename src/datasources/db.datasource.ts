86e5de659c8b02e9519d26ee05c7511a2ca793f3
TaulantLl-529119 <529119@student.fontys.nl>
2024-09-30 15:10:23 +0200

    omething

--- src/datasources/db.datasource.ts
+++ src/datasources/db.datasource.ts
@@ -1,12 +1,12 @@
 import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
 
-const config = {
-  name: 'db',
-  connector: 'memory',
-  url: 'postgresql://postgresdb_xvp7_user:BvqkqyNx22CitvqS8kk8VfdG6pDtK3Cb@dpg-crp8n008fa8c73du0avg-a.frankfurt-postgres.render.com/postgresdb_xvp7',
-  file: './data/db.json'
-};
+// const config = {
+//   name: 'db',
+//   connector: 'memory',
+//   url: 'postgresql://postgresdb_xvp7_user:BvqkqyNx22CitvqS8kk8VfdG6pDtK3Cb@dpg-crp8n008fa8c73du0avg-a.frankfurt-postgres.render.com/postgresdb_xvp7',
+//   file: './data/db.json'
+// };
 
 // Observe application's life cycle to disconnect the datasource when
 // application is stopped. This allows the application to be shut down
